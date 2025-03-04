9b5565321eaf157662f91396a2b556e6d670ebef
Miral Jandial <miraljn1997@yahoo.com>
2025-03-04 18:57:13 +0530

    MyPersonalEmployeeProject

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/employees")
+@EnableWebSecurity
 public class EmployeeController {
 
     @Autowired
